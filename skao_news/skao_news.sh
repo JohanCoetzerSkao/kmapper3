30bd10fbc46990ef4d3848186bf4b231c7f6af18
Johan Coetzer <johan.coetzer@tsolo.io>
2023-05-25 21:36:08 +0200

    Add crawl scipt for SKAO news.

---
+++ skao_news/skao_news.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+JFILE=skao_news.json
+rm -f $JFILE
+BDIR=./www.skao.int
+rm -rf $BDIR
+mkdir $BDIR
+echo "Write $JFILE"
+scrapy crawl spider -o $JFILE
+SITE="https://www.skao.int/en/news"
+
+echo "Read $JFILE"
+cat $JFILE | python -m json.tool | grep -iw "URL" | cut -d: -f2- | while read BURL
+do
+    FURL=$(echo $BURL | tr -d ',' | tr -d '"')
+    echo "Download $FURL"
+    FPATH=$FURL     # $(echo $BURL | cut -d/ -f4- | tr '-' '_' | tr -d ',' | tr -d '"')
+    FNAME=$(basename $FPATH)
+    DNAME=$(dirname $FPATH)
+    mkdir -p $BDIR/$DNAME
+    DL_NAME="${BDIR}/${DNAME}/${FNAME}.html"
+    SITE_URL="${SITE}/${FURL}"
+    echo "Crawl $SITE_URL"
+    echo "Write $DL_NAME"
+    # echo "Run> scrapy fetch --nolog $SITE_URL"
+    scrapy fetch --nolog $SITE_URL > $DL_NAME
+done
